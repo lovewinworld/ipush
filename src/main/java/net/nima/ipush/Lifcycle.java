a6de29adf405acde9d1074563b0dab8aba7a8801
lovewinworld <think@think-THINK.(none)>
2012-12-19 22:51:27 +0800

    change the directory

---
+++ src/main/java/net/nima/ipush/Lifcycle.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.nima.ipush;
+
+/**
+ * 组件的生命周期
+ * 
+ * @author Kevin Lee
+ *
+ */
+public interface Lifcycle {
+	
+	public void start();
+
+}
---
+++ src/main/java/net/nima/ipush/Server.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package net.nima.ipush;
+
+/**
+ * @author Kevin Lee
+ *
+ */
+public interface Server {
+
+}
